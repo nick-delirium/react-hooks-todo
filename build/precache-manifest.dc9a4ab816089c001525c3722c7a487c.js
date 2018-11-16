cc4ac7d062815a15cdfbd3f0c4e506a5b262cbb4
Nikita Melnikov <sylenien@gmail.com>
2018-11-17 00:31:22 +0300

    i dunno

---
+++ build/precache-manifest.dc9a4ab816089c001525c3722c7a487c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3f754b78c8b9c3b518b",
+    "url": "/react-hooks-todo/static/js/runtime~main.b3f754b7.js"
+  },
+  {
+    "revision": "227b5c864ccb229cf928",
+    "url": "/react-hooks-todo/static/js/main.227b5c86.chunk.js"
+  },
+  {
+    "revision": "5500fd3413a57f5e0c64",
+    "url": "/react-hooks-todo/static/js/1.5500fd34.chunk.js"
+  },
+  {
+    "revision": "227b5c864ccb229cf928",
+    "url": "/react-hooks-todo/static/css/main.a5275009.chunk.css"
+  },
+  {
+    "revision": "cb22f23d87278db9993d689382f37eed",
+    "url": "/react-hooks-todo/index.html"
+  }
+];
